8b15c942454584175aeb9d875bb75ec2eb414ae4
Mohammad Ansar <ansar.ofcl00@gmail.com>
2024-07-26 15:45:50 +0530

    feat : implement trpc

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,9 +1,14 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
 interface pageProps {}
+
+enum TErrCodes {
+  UNAUTHORIZED,
+}
 
 let Page: FC<pageProps> = ({}) => {
   let router = useRouter();
@@ -11,7 +16,47 @@ let Page: FC<pageProps> = ({}) => {
 
   let origin = searchParams.get("origin");
 
-  return <div>{origin}</div>;
+  /* api call to  authcall end point */
+
+  let { data, isLoading, error } = trpc.authCallback.useQuery(undefined, {
+    meta: { errCode: TErrCodes.UNAUTHORIZED },
+  });
+
+  // useEffect(() => {
+  //   if (error?.data?.code === "UNAUTHORIZED") {
+  //     router.push("/sign-in");
+  //   }
+  //   if (!isLoading) {
+  //     router.push(
+  //       data !== undefined && data.success && origin
+  //         ? `${origin}`
+  //         : "/dashboard"
+  //     );
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [data, isLoading]);
+
+  useEffect(() => {
+    if (error?.data?.code === "UNAUTHORIZED") router.push("/sign-in");
+    else if (!isLoading)
+      router.push(
+        data !== undefined && data.success && origin
+          ? `${origin}`
+          : "/dashboard"
+      );
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, isLoading]);
+
+  return (
+    <div className=" w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account ....</h3>
+        <p>You will be redirected autometically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- trpc/index.ts
+++ trpc/index.ts
@@ -1,7 +1,39 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+import { use } from "react";
 export const appRouter = router({
-  test: publicProcedure.query(() => {
-    return 1;
+  authCallback: publicProcedure.query(async () => {
+    let { getUser } = getKindeServerSession();
+    let user = await getUser();
+
+    if (!user?.id || !user?.email) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+      });
+    }
+
+    /*  check user in db */
+
+    let dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      /* create user */
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+    return {
+      success: true,
+    };
   }),
 });
 
